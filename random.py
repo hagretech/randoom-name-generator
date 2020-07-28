20c140205b7f03df5cba8cc0086a9b637530228f
hagretech <68892112+hagretech@users.noreply.github.com>
2020-07-28 14:56:40 +0300

    initalaizing

---
+++ random.py
@@ -0,0 +1,13 @@
+## import package
+import random as rav
+## define varables
+list = ["a","b","c","d","s","d","f","h"]
+violes = ["a","e","i","o","u"]
+## make your function
+def gen():
+    
+    for i in range(10):
+        name = rav.choice([list])
+    print(name)
+## call your functions
+gen()
